7306e1504db69200915331cd8891d9fe36705967
unknown <eshanesn97@gmail.com>
2025-02-13 14:52:40 +0530

    SEO CHECK-04

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    Sitemap({
-      // Define your website's base URL
-      hostname: 'https://villa-elegant-oasis.netlify.app/',
-      // List all routes in your React app
-    }),
-  ],
+  plugins: [react()],
 })
