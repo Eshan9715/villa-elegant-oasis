a208ecd979ba540f20b1504927095422ed53b1ff
unknown <eshanesn97@gmail.com>
2025-02-13 10:53:22 +0530

    Tasks & Functions Done-3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    Sitemap({
+      // Define your website's base URL
+      hostname: 'https://villa-elegant-oasis.netlify.app/',
+      // List all routes in your React app
+    }),
+  ],
 })
